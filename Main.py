5a3a1f674176a09ebbd2d316d85b9804fa97f95c
ReynaldTM <137812640+ReynaldTM@users.noreply.github.com>
2024-07-16 16:54:47 -0500

    Beginning Data_Entry.py
    Tested Main.py
    Setting need function in Data_Entry.py

--- Main.py
+++ Main.py
@@ -6,23 +6,35 @@ import pandas as pd
 import csv
 from datetime import datetime
 
+
 class CSV:
     CSV_FILE = "finance_data.csv"  # class variable
+    COLUMNS = ["date", "amount", "category", "description"]
 
-    @classmethod # will have access to class itself, but not its instance
+    @classmethod  # will have access to class itself, but not its instance
     # meaning it has access to other class methods and class variables
     def initialize_csv(cls):
-        try: # "pd" are pandas
-            pd.read_csv(cls.CSV_FILE) # trying to find an existing CSV file
+        try:  # "pd" are pandas
+            pd.read_csv(cls.CSV_FILE)  # trying to find an existing CSV file
         except FileNotFoundError:
-            df = pd.DataFrame(columns=["date", "amount", "category", "description"]) # making the csv file with columns
-            df.to_csv(cls.CSV_FILE, index=False) # saving a local file with the class variable name above
+            df = pd.DataFrame(columns=cls.COLUMNS)  # making the csv file with columns
+            df.to_csv(cls.CSV_FILE, index=False)  # saving a local file with the class variable name above
+
+    @classmethod
+    def add_entry(cls, date, amount, category, description):  # new class method taking new data
+        new_entry = {
+            "date": date,
+            "amount": amount,
+            "category": category,
+            "description": description
+        }  # new data created as a dictionary
+        with open(cls.CSV_FILE, "a", newline="") as csvfile:  # context manager,
+            # automatically handles file closing, memory leaks, etc.
+            # opened CSV file in append mode
+            writer = csv.DictWriter(csvfile, fieldnames=cls.COLUMNS)  # writing diction into CSV file
+            writer.writerow(new_entry)  # new data being written into their own rows according to columns
+            print("Entry added successfully")
+
 
 CSV.initialize_csv()
-
-
-
-
-
-
-
+CSV.add_entry("20-07-24", 125.65, "Income", "salary")
--- data_entry.py
+++ data_entry.py
@@ -1,3 +1,29 @@
 # for data from user will contain:
 # input statements
 # validating data
+
+
+from datetime import datetime
+
+def get_date(prompt, allow_default=False):# prompt is input is question before date come
+                                                                   # for getting date in multiple different places
+                                                                   # allow_default = will select current date by hitting return.
+                                                                   # since its set to False, date will be required
+    date_str = input(prompt)
+    if allow_default and not date_str: # allow_default is True and date_str is None
+        return datetime.today().strftime("%d-%m-%Y") # return current date at format, date, month, year
+    try:
+        valid_date = datetime.strftime(date_str,"%d-%m-%Y")
+        return valid_date.strftime(date_str,"%d-%m-%Y")
+    except ValueError:
+        print("Invalid date format. Please enter date in dd-mm-yyyy format")
+        return get_date(prompt, allow_default=False)
+
+def get_amount():
+    pass
+
+def get_category():
+    pass
+
+def get_description():
+    pass
