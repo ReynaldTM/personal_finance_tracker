36f3ad87e07eb0df8d97b51021149718bfdce9d6
ReynaldTM <137812640+ReynaldTM@users.noreply.github.com>
2024-07-16 15:34:16 -0500

    Started new project
    Made class
    Made class method that makes CSV file

--- Main.py
+++ Main.py
@@ -12,10 +12,17 @@ class CSV:
     @classmethod # will have access to class itself, but not its instance
     # meaning it has access to other class methods and class variables
     def initialize_csv(cls):
-        try:
-            pd.read_csv(cls.CSV_FILE)
+        try: # "pd" are pandas
+            pd.read_csv(cls.CSV_FILE) # trying to find an existing CSV file
         except FileNotFoundError:
-            df = pd.DataFrame(columns=["date", "amount", "category", "description"])
+            df = pd.DataFrame(columns=["date", "amount", "category", "description"]) # making the csv file with columns
+            df.to_csv(cls.CSV_FILE, index=False) # saving a local file with the class variable name above
+
+CSV.initialize_csv()
+
+
+
+
 
 
 
