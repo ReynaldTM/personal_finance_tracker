32080804cb0f65390d8918a2845fbe16c13a247b
ReynaldTM <137812640+ReynaldTM@users.noreply.github.com>
2024-07-18 16:59:36 -0500

    Finished graph plotting

--- Main.py
+++ Main.py
@@ -5,6 +5,7 @@ import pandas as pd
 import csv
 from datetime import datetime
 from data_entry import get_date, get_amount, get_category, get_description
+import matplotlib.pyplot as plt
 
 
 class CSV:
@@ -52,16 +53,16 @@ class CSV:
         # locating different rows that mask matches then return filtered dataframe
 
         if filtered_df.empty:
-            print("No transactions found in given date range")
+            print("No transactions found in given date range.")
         else:
-            print(f"Transaction from {start_date.strftime (CSV.FORMAT)} to {end_date.strftime (CSV.FORMAT)}"
+            print(f"Transaction from {start_date.strftime(CSV.FORMAT)} to {end_date.strftime(CSV.FORMAT)}"
                   )
 
             print(
                 filtered_df.to_string(
                     index=False, formatters={"date": lambda x: x.strftime(CSV.FORMAT)}
-                                        )
-                  )
+                )
+            )
             # key & pair, date is key. Function is pair using lambda as pair is calling all entry in date column
 
             total_income = filtered_df[filtered_df["category"] == "Income"]["amount"].sum()
@@ -69,7 +70,9 @@ class CSV:
             print("\nSummary: ")
             print(f"Total Income: ${total_income:.2f}")
             print(f"Total Expense: ${total_expense:.2f}")
-            print(f"Net Savings: ${(total_income-total_expense):.2f}")
+            print(f"Net Savings: ${(total_income - total_expense):.2f}")
+
+        return filtered_df
 
 
 def add():
@@ -81,10 +84,36 @@ def add():
     description = get_description()
     CSV.add_entry(date, amount, category, description)
 
-def plot_transactions(df):
-    df. set_index("date", inplace=True)
 
-    income_df = df[df["category"] == "Income"].resample("D")
+def plot_transactions(df):
+
+    df.sort_values(by="date", inplace=True)
+    df.set_index("date", inplace=True)
+    # index is the way to locate and manipulate rows,  modifying df in place
+
+    income_df = (df[df["category"] == "Income"]
+         .resample("D")
+         .sum()
+         .reindex(df.index, fill_value=0))
+    expense_df = (df[df["category"] == "Expense"]
+         .resample("D")
+         .sum()
+         .reindex(df.index, fill_value=0))
+    # .resample makes smoother lines on graph "D" stand for daily frequency, filling empty row
+    # sum values aggregates rows that have same dates and add amount together
+    # reindex make sure it conforms to index of df, fill_value just fills in day with 0
+
+    plt.figure(figsize=(10, 5))
+    plt.plot(income_df.index, income_df["amount"], label="Income", color="g")
+    plt.plot(expense_df.index, expense_df["amount"], label="Expense", color="r")
+    plt.xlabel("Date")
+    plt.ylabel("amount")
+    plt.title("Income and Expenses Over Time")
+    plt.legend()
+    plt.legend()
+    plt.grid(True)
+    plt.show()
+
 
 def main():
     while True:
@@ -98,12 +127,15 @@ def main():
         elif choice == "2":
             start_date = get_date("Enter the star date (dd-mm-yyyy): ")
             end_date = get_date("Enter the end date (dd-mm-yyyy): ")
-            df = CSV.get_transaction(start_date,end_date)
+            df = CSV.get_transaction(start_date, end_date)
+            if input("Do see a plot? (y/n) ").lower() == "y":
+                plot_transactions(df)
         elif choice == "3":
             print("Exiting")
             break
         else:
             print("Invalid choice.  Enter 1, 2,  or3.")
+
 
 if __name__ == "__main__":
     main()
